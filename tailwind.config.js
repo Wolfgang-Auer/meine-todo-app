816fb550f489006201c3e1fc7141dd9026267755
Wolfgang Auer <auer.wolf@gmail.com>
2025-04-16 17:22:14 +0200

    Erste Version der Todo-App

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Stelle sicher, dass dieser Pfad deine Projektstruktur widerspiegelt
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
